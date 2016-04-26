36e897006d20a96ffb6f5cd1109f77a0d280cde0
LimpLungs <xlaxlimplungs@gmail.com>
2016-04-26 01:05:24 -0600

    Edited version number, after forgetting twice now.

--- src/main/java/com/limplungs/invcalc/help/Reference.java
+++ src/main/java/com/limplungs/invcalc/help/Reference.java
@@ -5,7 +5,7 @@ import net.minecraft.client.Minecraft;
 public class Reference
 {
 	public static final String MODID = "invcalc";
-	public static final String VERSION = "1.0.0dev4";
+	public static final String VERSION = "1.0.0dev6";
 	
 	
 	public static String getDigitalTime()
